078c5c6ed33abcc9f281d6891deb4275d41a49b7
vanyachyzh <ivanchyzh97@gmai.com>
2022-07-11 22:21:38 +0300

    for mobile

--- src/components/result/result.js
+++ src/components/result/result.js
@@ -8,42 +8,45 @@ import { useSelector } from "react-redux";
 function Result() {
     let [currentCar, setCurrentCar] = useState([])
     let vinNumber = useSelector(state => state.vin.vin)
-    let history = useSelector(state=> state.vin.history)
+    let history = useSelector(state => state.vin.history)
 
     useEffect(() => {
-        fetch("https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/"+vinNumber+"?format=json")
+        fetch("https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/" + vinNumber + "?format=json")
             .then(r => r.json())
             .then(r => {
                 setCurrentCar(r.Results.filter((e) => e.Value !== null && e.Value !== "" && e.Value !== 0 && e.Value !== "0" && e.Variable !== "Error Text"))
             })
-    },[vinNumber])
+    }, [vinNumber])
 
-    function handlerHistory (e){
-        fetch("https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/"+e.target.innerText+"?format=json")
-        .then(r => r.json())
-        .then(r => {
-            setCurrentCar(r.Results.filter((e) => e.Value !== null && e.Value !== "" && e.Value !== 0 && e.Value !== "0" && e.Variable !== "Error Text"))
-        })
+    function handlerHistory(e) {
+        fetch("https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/" + e.target.innerText + "?format=json")
+            .then(r => r.json())
+            .then(r => {
+                setCurrentCar(r.Results.filter((e) => e.Value !== null && e.Value !== "" && e.Value !== 0 && e.Value !== "0" && e.Variable !== "Error Text"))
+            })
     }
-    
+
+    var five = ["1FTFW1CT5DFC10312", "1FTFW1CT5DFC10312", "1FTFW1CT5DFC10312", "1FTFW1CT5DFC10312", "1FTFW1CT5DFC10312"]
     return (
         <>
             <div className="content">
 
                 <ul className="recent-results">
                     {history.map((e) => {
-                        return (<li className="historyResult" onClick={(e)=>{handlerHistory(e)}}  key={Math.random()}>
+                        return (<li className="historyResult" onClick={(e) => { handlerHistory(e) }} key={Math.random()}>
                             <div >{e}</div>
                         </li>)
                     })}
-                </ul> 
+                </ul>
 
                 {currentCar.map((e) => {
-                        return (<div className="item" key={Math.random()}>
-                            <div className="variable">{e.Variable}:</div>
-                            <div className="value">{e.Value}</div>
-                        </div>)
-                    })}
+                    return (<div className="item" key={Math.random()}>
+                        <div className="variable">{e.Variable}:</div>
+                        <div className="value">{e.Value}</div>
+                    </div>)
+                })}
+
+
 
                 <div className="vl"></div>
             </div>
